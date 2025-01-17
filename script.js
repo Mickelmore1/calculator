41ce8cb671f44cf8690348ce61559171722e5661
Mickelmore1 <190520509+Mickelmore1@users.noreply.github.com>
2025-01-17 23:43:02 +0000

    Added basic calculator functionality. Next step is looking at edge cases.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
-const power = (a, b) => a ** b;
+const add = (a, b) => Number(b) + Number(a);
+const subtract = (a, b) => Number(b) - Number(a);
+const multiply = (a, b) => Number(b) * Number(a);
+const divide = (a, b) => Number(b) / Number(a);
+const power = (a, b) => Number(b) ** Number(a);
 const factorial = (value) => {
   product = 1;
   for (x = value; x > 0; x--) {
@@ -11,98 +11,80 @@ const factorial = (value) => {
   return product;
 };
 
-let number1;
-let number2 = 0;
-let operator;
-let sum = 0;
+function operate(operator, numberA, numberB) {
+    console.log(operator);
+    console.log(numberA);
+    console.log(numberB);
+    if (operator == '+') { calculator.numberA = add(numberA, numberB) };
+    if (operator == '-') { calculator.numberA = subtract(numberA, numberB) };
+    if (operator == '*') { calculator.numberA =  multiply(numberA, numberB) };
+    if (operator == '/') { calculator.numberA = divide(numberA, numberB) };
+    return getCalculatorDisplay.value = calculator.numberA;
+}
 
-let calculation = [];
+let calculator = {
+    numberA: 0,
+    numberB: 0,
+    operator: undefined,
+    display: 0,
+}
+
+const getCalculatorDisplay = document.getElementById('calculator-display');
+getCalculatorDisplay.value = calculator.display;
 
 
+ 
 
+function updateDisplay(){
+    const buttonNumList = document.querySelectorAll('.num');   
+    buttonNumList.forEach(numButton => {
+        numButton.addEventListener('click', () => {
 
+            
+            if(calculator.numberA == 0){ calculator.numberA = ''};
+            
+            calculator.numberA += numButton.textContent;
+            getCalculatorDisplay.value = calculator.numberA;
+            console.log(calculator.numberA)
+        })
+        
+    });
+}
 
-function operate() {
-    console.log("number 1 before operation is: " + number1);
-    console.log("number 2 before operation is: " + number2);
-    if (operator == '+'){
-        number2 = add(number2, number1)
-    }
-    if (operator == '-'){
-        number2 = subtract(number2, number1)
-    }
-    if (operator == '*'){
-        number2 = multiply(number2, number1)
-    }
-    if (operator == '/'){
-        number2 = divide(number2, number1)
-    }
-    console.log(number2)
-    sum = number2
-    return Number(sum)
+function updateOperator(){
+    const buttonOperatorList = document.querySelectorAll('.operator');
+    buttonOperatorList.forEach(operatorButtonPress => {
+        operatorButtonPress.addEventListener('click', () => {
+            calculator.operator = operatorButtonPress.textContent;
+            calculator.numberB = calculator.numberA;
+            calculator.numberA = 0;
+         
+        })
+    })
+}
+
+function pressEqual(){
+    const equalButton = document.getElementById('button-equal')
+    equalButton.addEventListener('click', () => {
+        console.log(calculator);
+        operate(calculator.operator, calculator.numberA, calculator.numberB); 
+        calculator.operator = undefined;
+    })
+}
+
+function pressAC(){
+    const acButton = document.getElementById('button-AC')
+    acButton.addEventListener('click', () => {
+        calculator.numberA = 0;
+        calculator.numberB = 0;
+        calculator.operator = undefined;
+        getCalculatorDisplay.value = calculator.numberA;
+    })
 }
 
 
-   
 
-const buttonNumList = document.querySelectorAll('.num');   
-const buttonOperatorList = document.querySelectorAll('.operator'); 
-
-
-document.getElementById('button-equal').addEventListener('click', () => { 
-    document.getElementById('calculator-display').textContent = operate(); 
-}); 
-
-buttonNumList.forEach(button => {
-    document.getElementById(button.id).addEventListener('click', () => { 
-        number1 = Number(document.getElementById(button.id).textContent);
-        updateDisplay(button.id);
-    });
-});
-
-
-//Operators Update
-buttonOperatorList.forEach(button => {
-    document.getElementById(button.id).addEventListener('click', () => { 
-        operator = document.getElementById(button.id).textContent;
-       
-    }); 
-    operate();
-});
-
-
-//AC Button
-document.getElementById('button-AC').addEventListener('click', () => {
-    document.getElementById('calculator-display').textContent = 0;
-    number1 = 0;
-    number2 = 0;
-})
-
-
-
-function updateDisplay(buttonPressed){
-
-    console.log(buttonPressed)
-    document.getElementById('calculator-display').textContent = document.getElementById(buttonPressed).textContent; 
-    };
-
-/*     document.getElementById('button-AC').addEventListener('click', test);
-    document.getElementById('button-plusminus').addEventListener('click', test);
-    document.getElementById('button-percentage').addEventListener('click', test);
-    document.getElementById('button-divide').addEventListener('click', test);
-    document.getElementById('button-7').addEventListener('click', test);
-    document.getElementById('button-8').addEventListener('click', test);
-    document.getElementById('button-9').addEventListener('click', test);
-    document.getElementById('button-multiply').addEventListener('click', test);
-    document.getElementById('button-4').addEventListener('click', test);
-    document.getElementById('button-5').addEventListener('click', test);
-    document.getElementById('button-6').addEventListener('click', test);
-    document.getElementById('button-minus').addEventListener('click', test);
-    document.getElementById('button-1').addEventListener('click', test);
-    document.getElementById('button-2').addEventListener('click', test);
-    document.getElementById('button-3').addEventListener('click', test);
-    document.getElementById('button-plus').addEventListener('click', test);
-    document.getElementById('button-0').addEventListener('click', test);
-    document.getElementById('button-decimal').addEventListener('click', test);
-    document.getElementById('button-equal').addEventListener('click', test);
- */
+updateDisplay();
+updateOperator();
+pressEqual();
+pressAC();
